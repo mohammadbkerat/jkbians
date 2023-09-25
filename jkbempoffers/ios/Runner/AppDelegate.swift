cf2836640ee417154e9fbc84832d9cc40aa8c357
mohammadbkerat <85095027+mohammadbkerat@users.noreply.github.com>
2023-09-25 22:41:56 +0300

    Update AppDelegate.swift

--- jkbempoffers/ios/Runner/AppDelegate.swift
+++ jkbempoffers/ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
+
     _ application: UIApplication,
+
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+
   ) -> Bool {
+
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func application(_ application:UIApplication,didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Date){
+
+        Messaging.messaging().apnsToken = deviceToken
+
+        print("Token: \(deviceToken)")
+
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+
+    }
+
 }
